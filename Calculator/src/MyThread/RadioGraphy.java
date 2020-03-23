211d47a0563dd35f268b49aec6999f3a9ede036e
ManiKatwal <62191180+ManiKatwal@users.noreply.github.com>
2020-03-23 11:19:32 -0500

    Remote File added

--- Calculator/src/MyThread/RadioGraphy.java
+++ Calculator/src/MyThread/RadioGraphy.java
@@ -1,10 +1,10 @@
 package MyThread;
 
-public class DemoAAdityaa {
+public class RadioGraphy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+
 	}
 
 }
